be514c7af317831f747662403f53a7692830157c
Matteo Monti <matteo.monti@rain.vg>
2017-05-05 15:17:46 +0200

    Update watcher.js
    Basic response handling for count.

--- dns/watcher.js
+++ dns/watcher.js
@@ -20,11 +20,30 @@ module.exports = function(root)
     server.on('request', function(request, response)
     {
       for(var i = 0; i < request.question.length; i++)
-        console.log(request.question[i].name);
+        if(request.question[i].name == domains.count)
+        {
+          response.answer.push(dns.A({
+            name: domains.count,
+            address: inttoip(count),
+            ttl: 60
+          }));
+        }
+
+      response.send();
+    });
+
+    server.on('error', function(error, buffer, request, response)
+    {
+      console.log(error.stack);
     });
 
     server.serve(port);
   };
 
+  // Private methods
 
+  var inttoip = function(value)
+  {
+    return [(value >> 24) & 0xff, (value >> 16) & 0xff, (value >> 8) & 0xff, value & 0xff].join('.');
+  };
 };
